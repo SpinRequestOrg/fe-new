25cbaaf657c15e876e542dee81551fbc08a96dbb
Rilwan <akinyusuf5@gmail.com>
2024-11-02 17:11:05 +0100

    removed middleware

---
+++ .output/public/_nuxt/DRU9GjLE.js
@@ -0,0 +1,1 @@
+import{aZ as t}from"./BP-Jkhhp.js";const r=t(e=>{});export{r as default};
--- middleware/host.ts
+++ middleware/host.ts
@@ -1,14 +1,8 @@
 export default defineNuxtRouteMiddleware((to) => {
-  const nuxtApp = useNuxtApp();
-  if (
-    import.meta.client &&
-    nuxtApp.isHydrating &&
-    nuxtApp.payload.serverRendered
-  )
-    return;
-  const { auth_user } = useAuth();
-  const role = auth_user.value?.role;
-  if (role !== "host" && to.path !== "/") {
-    return navigateTo("/");
-  }
+  return;
+  // const { auth_user } = useAuth();
+  // const role = auth_user.value?.role;
+  // if (role !== "host" && to.path !== "/") {
+  //   return navigateTo("/");
+  // }
 });
