f605fc83a9504302cbdf632752835bf19ccd0be2
Damilola Ogunsakin <ogunsakin191@gmail.com>
2024-12-08 05:05:35 +0000

    Patch

---
+++ .output/public/_nuxt/Bl1fQot1.js
@@ -0,0 +1,1 @@
+import{b7 as a,i as s,x as r}from"./DcGHPZRe.js";const l=a(t=>{var e;const{auth_user:o}=s();if(((e=o.value)==null?void 0:e.role)!=="host"&&t.path!=="/")return r("/")});export{l as default};
