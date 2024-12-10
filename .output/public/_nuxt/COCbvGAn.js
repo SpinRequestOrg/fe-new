8e4c9468d53a87b00f7cd0bea5f1df3f16c4f91c
Rilwan <akinyusuf5@gmail.com>
2024-12-10 08:08:27 +0100

    fixes

--- .output/public/_nuxt/COCbvGAn.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:.output/public/_nuxt/COCbvGAn.js
-import{o as e,c as r}from"./CKUYctw9.js";function t(c,o){return e(),r("svg")}const a={render:t};export{a as default,t as render};
-========
-import{o as e,c as r}from"./B3oCP8tS.js";function t(c,o){return e(),r("svg")}const a={render:t};export{a as default,t as render};
->>>>>>>> 1284d1b7c268752b39346e9194f5fedd89983677:.output/public/_nuxt/0yohd33k.js
